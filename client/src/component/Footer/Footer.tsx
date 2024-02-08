29690b62af72975e6add6cb41c6acb8588ef7fca
Bazl1 <dazman3243243@gmail.com>
2024-02-08 15:07:09 +0200

    Creating a project, folders

---
+++ client/src/component/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import s from "./Footer.module.scss";
+
+const Footer = () => {
+    return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ client/src/component/LayoutMain/LayoutMain.tsx
@@ -0,0 +1,9 @@
+const LayoutMain = ({ children }: any) => {
+    return (
+        <>
+            <main className="main">{children}</main>
+        </>
+    );
+};
+
+export default LayoutMain;
