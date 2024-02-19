bfc8e7cd5c07d4104114e190c8e6a0a507b3d3d0
ReZerVV <kirillmorozov547@gmail.com>
2024-02-19 16:21:24 +0200

    fix: icons url

--- server/src/Modules/Flipster.Modules.Adverts/Data/Seeds/CategorySeed.cs
+++ server/src/Modules/Flipster.Modules.Adverts/Data/Seeds/CategorySeed.cs
@@ -9,13 +9,13 @@ public class CategorySeed
     {
         using var _db = serviceProvider.GetRequiredService<AdvertsDbContext>();
         _db.Categories.AddRange(
-            new Category(1, "Free", "free-icon.svg"),
-            new Category(2, "Cars", "cars-icon.svg"),
-            new Category(3, "Works", "works-icon.svg"),
-            new Category(4, "Animals", "animals-icon.svg"),
-            new Category(5, "Electronics", "electronics-icon.svg"),
-            new Category(6, "Clothes", "clothes-icon.svg"),
-            new Category(7, "Business", "business-icon.svg"));
+            new Category(1, "Free", "http://localhost:5247/icons/free-icon.svg"),
+            new Category(2, "Cars", "http://localhost:5247/icons/cars-icon.svg"),
+            new Category(3, "Works", "http://localhost:5247/icons/works-icon.svg"),
+            new Category(4, "Animals", "http://localhost:5247/icons/animals-icon.svg"),
+            new Category(5, "Electronics", "http://localhost:5247/icons/electronics-icon.svg"),
+            new Category(6, "Clothes", "http://localhost:5247/icons/clothes-icon.svg"),
+            new Category(7, "Business", "http://localhost:5247/icons/business-icon.svg"));
         _db.SaveChanges();
     }
 }
