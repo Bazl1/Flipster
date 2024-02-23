4b0aafe9b3c03a4cc3e9d21d185b8a81cc396325
ReZerVV <kirillmorozov547@gmail.com>
2024-02-23 15:33:08 +0200

    fix(locations): the property "value" will be rename to "label"

--- server/src/Flipster.Modules.Users/Dtos/LocationDto.cs
+++ server/src/Flipster.Modules.Users/Dtos/LocationDto.cs
@@ -3,5 +3,5 @@ namespace Flipster.Modules.Users.Dtos;
 public class LocationDto
 {
     public required string Id { get; set; }
-    public required string Value { get; set; }
+    public required string Label { get; set; }
 }
